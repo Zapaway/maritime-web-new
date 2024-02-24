6de20817a2c89c3ab18d1327784d3dd2b5da0b8f
Kenneth Chap <53070266+Zapaway@users.noreply.github.com>
2024-02-24 10:14:55 -0500

    :sparkles: earth

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import './App.css'
-import Globe from 'react-globe.gl'
+import "./App.css";
+import Globe from "react-globe.gl";
 
 function App() {
-
   return (
-<main>
-  <div>
-    <Globe          globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
-/>
-  </div>
-</main>
-  )
+    <main>
+      <Globe globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"/>
+    </main>
+  );
 }
 
-export default App
+export default App;
